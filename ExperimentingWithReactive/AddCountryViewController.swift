b96f64f90877b7075f6e9d9b8848778523ae8488
manoj.gubba <manoj.gubba@mercari.com>
2018-11-05 16:36:11 +0900

    minor refactoring

--- ExperimentingWithReactive/AddCountryViewController.swift
+++ ExperimentingWithReactive/AddCountryViewController.swift
@@ -38,33 +38,31 @@ class AddCountryViewController: UITableViewController {
  
     override func viewDidLoad() {
         super.viewDidLoad()
-                
+        
         title = "Add Country"
         configureDoneButton()
     }
     
     func configureDoneButton(){
+        let countrySignal = countryText.reactive.continuousTextValues
+        let vatSignal = vatText.reactive.continuousTextValues
         
-        let text1 = Property(initial: "", then: countryText.reactive.continuousTextValues)
-        let text2 = Property(initial: "", then: vatText.reactive.continuousTextValues)
+        let countryProperty = Property(initial: "", then: countrySignal)
+        let vatProperty = Property(initial: "", then: vatSignal)
         
+        let isVatValidSignal = vatSignal.map {self.isVatValid(in: $0!)}
         
-        let text = vatText.reactive.continuousTextValues.map { (text) -> Bool in
-            if(text == ""){
-                return true
-            }
-            guard let number = Float(text!) else{
-            return false
-            }
-            if (number <= 100 && number >= 0){
-                return true
-            }else {return false}
-        }
+        let property0 = Property.combineLatest(countryProperty, vatProperty).map {!(($0?.isEmpty)!||($1?.isEmpty)!)}
+        let property1 = Property(initial: true, then: isVatValidSignal)
         
-        let property1 = Property(initial: false, then: text)
-        let property = Property.combineLatest(text1, text2).map {!(($0?.isEmpty)!||($1?.isEmpty)!)}
-        let enable = Property.combineLatest(property, property1).map{$0&&$1}
+        let enable = Property.combineLatest(property0, property1).map{$0 && $1}
         doneOutlet.reactive.isEnabled <~ enable
-        validLabel.reactive.isHidden <~ text
+        validLabel.reactive.isHidden <~ isVatValidSignal
+    }
+    
+    func isVatValid(in text:String) -> Bool{
+        if(text.isEmpty){return true}
+        guard let number = Float(text) else{return false}
+        if (number <= 100 && number >= 0){return true} else {return false}
     }
 }
